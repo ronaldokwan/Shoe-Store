1a429ee991f76e68a185f1be44b19c308e034999
Ronaldo Kwan <ronaldokwan17@gmail.com>
2024-02-15 12:27:08 +0700

    save6

--- models/model.js
+++ models/model.js
@@ -5,13 +5,13 @@ class Model {
   static async validation(name, minSize, maxSize) {
     try {
       let errorMessage = [];
-      if (name.split(" ").length <= 1) {
+      if (name.split(" ").filter((n) => n).length <= 1) {
         errorMessage.push("name minimal 2 kata");
       }
-      if (typeof minSize !== "number" || typeof maxSize !== "number") {
+      if (minSize != Number(minSize) && maxSize != Number(maxSize)) {
         errorMessage.push("minSize dan maxSize must be number");
       }
-      if (minSize >= maxSize) {
+      if (Number(minSize) >= Number(maxSize)) {
         errorMessage.push("maxSize harus lebih besar dari minSize");
       }
       if (errorMessage.length !== 0) {
@@ -38,7 +38,7 @@ class Model {
   }
   static async root() {
     try {
-      let query = `SELECT s."id",s."name",c."name" as "categoryId",s."minSize",s."maxSize",s."status" FROM "Shoes" s JOIN "Categories" c ON c."id" = s."categoryId"`;
+      let query = `SELECT s."id",s."name",c."name" as "categoryId",s."minSize",s."maxSize",s."status" FROM "Shoes" s JOIN "Categories" c ON c."id" = s."categoryId" ORDER BY s."id"`;
       let { rows } = await pool.query(query);
       let data = rows.map((item) => {
         let { id, name, categoryId, minSize, maxSize, status } = item;
@@ -71,14 +71,7 @@ class Model {
   }
   static async addProcess(body) {
     try {
-      let errorMessage = await this.validation(
-        body.name,
-        body.minSize,
-        body.maxSize
-      );
-      if (errorMessage.length !== 0) {
-        throw errorMessage;
-      }
+      await this.validation(body.name, body.minSize, body.maxSize);
       let query = `INSERT INTO "Shoes" ("name","categoryId","minSize","maxSize","status") VALUES ('${body.name}','${body.categoryId}','${body.minSize}','${body.maxSize}','${body.status}')`;
       await pool.query(query);
     } catch (error) {
@@ -104,16 +97,13 @@ class Model {
   static async delete(id) {
     try {
       let valid = `SELECT s."status" FROM "Shoes" s WHERE "id" = '${id}'`;
+      let status;
       let { rows } = await pool.query(valid);
       for (let i = 0; i < rows.length; i++) {
         const element = rows[i];
         status = element.status;
       }
-      console.log(status);
-      let errorMessage = await this.deleteValidation(status);
-      if (errorMessage.length !== 0) {
-        throw errorMessage;
-      }
+      await this.deleteValidation(status);
       let query = `DELETE FROM "Shoes" WHERE "id" = '${id}'`;
       await pool.query(query);
     } catch (error) {
